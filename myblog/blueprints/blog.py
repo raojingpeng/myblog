6a1a9c59a507afff749d1f98cdca843e2a5d2329
raojingpeng <withrjp@gmail.com>
2019-01-28 18:21:49 +0800

    add tooltip

--- myblog/blueprints/blog.py
+++ myblog/blueprints/blog.py
@@ -21,3 +21,23 @@ def index():
     posts = pagination.items
     categories = Category.query.order_by(Category.id).all()
     return render_template('blog/index.html', pagination=pagination, categories=categories, posts=posts)
+
+
+@blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    page = request.args.get('page', 1)
+    per_page = current_app.config['POST_PER_PAGE']
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
+    posts = pagination.items
+    categories = Category.query.order_by(Category.id).all()
+    return render_template('blog/index.html', pagination=pagination, categories=categories, posts=posts)
+
+
+@blog_bp.route('/category/<int:category_id>')
+def show_category(category_id):
+    page = request.args.get('page', 1)
+    per_page = current_app.config['POST_PER_PAGE']
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
+    posts = pagination.items
+    categories = Category.query.order_by(Category.id).all()
+    return render_template('blog/index.html', pagination=pagination, categories=categories, posts=posts)
--- myblog/static/js/init.js
+++ myblog/static/js/init.js
@@ -2,5 +2,10 @@
   $(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
+    function render_time() {
+        return moment($(this).data('timestamp')).format('lll')
+    }
+
+    $('.tooltipped').tooltip();
   });
 })(jQuery);
