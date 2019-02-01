86af5dc37459dca9a9c5e67cf7d4eaf5db015034
raojingpeng <withrjp@gmail.com>
2019-02-01 18:57:46 +0800

    add category

--- myblog/blueprints/blog.py
+++ myblog/blueprints/blog.py
@@ -28,6 +28,16 @@ def about():
     return render_template('blog/about.html')
 
 
+@blog_bp.route('/category/<category_name>')
+def show_category(category_name):
+    category = Category.query.filter_by(name=category_name).get_or_404()
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['POST_PER_PAGE']
+    pagination = Post.query.with_parent(category).order_by(Post.timestamp.desc()).pagination(page, per_page)
+    posts = pagination.items
+    return render_template('blog/category.html', category=category, pagination=pagination, posts=posts)
+
+
 @blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def show_post(post_id):
     page = request.args.get('page', 1)
@@ -37,12 +47,3 @@ def show_post(post_id):
     categories = Category.query.order_by(Category.id).all()
     return render_template('blog/index.html', pagination=pagination, categories=categories, posts=posts)
 
-
-@blog_bp.route('/category/<int:category_id>')
-def show_category(category_id):
-    page = request.args.get('page', 1)
-    per_page = current_app.config['POST_PER_PAGE']
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
-    posts = pagination.items
-    categories = Category.query.order_by(Category.id).all()
-    return render_template('blog/index.html', pagination=pagination, categories=categories, posts=posts)
--- myblog/static/js/init.js
+++ myblog/static/js/init.js
@@ -1,10 +1,16 @@
-(function($){
-  $(function(){
-    $('.sidenav').sidenav();
-    $('.collapsible').collapsible();
-    $('.tooltipped').map(function () {
-        $(this).attr('data-tooltip', moment($(this).data('timestamp')).format('YYYY/MM/DD HH:mm:ss'));
-      });
-    $('.tooltipped').tooltip();
-  });
-})(jQuery);
+(function ($) {
+	$(function () {
+		// Render title
+		let active_title = $('#slide-out li.active:first-child a').clone();
+		active_title.find('i').remove();
+		$('.top-nav h3.header').text($.trim(active_title.text()));
+
+		// Initialize components
+		$('.sidenav').sidenav();
+		$('.collapsible').collapsible();
+		$('.tooltipped').map(function () {
+			$(this).attr('data-tooltip', moment($(this).data('timestamp')).format('YYYY/MM/DD HH:mm:ss'));
+		});
+		$('.tooltipped').tooltip();
+	});
+})(jQuery)
